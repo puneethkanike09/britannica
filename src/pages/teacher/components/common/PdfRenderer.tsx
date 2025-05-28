07d75027a8a525818d6e54f3237c83d08429f45f
puneethkanike09 <reachout.puneeth@gmail.com>
2025-05-28 16:29:06 +0530

    new changes

--- src/pages/teacher/components/common/PdfRenderer.tsx
+++ src/pages/teacher/components/common/PdfRenderer.tsx
@@ -4,10 +4,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import { X } from "lucide-react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 interface PdfRendererProps {
     file: string | null;
