42469775ddce84f0802a9e68ed591d8de15fa409
puneethkanike09 <reachout.puneeth@gmail.com>
2025-06-12 10:27:03 +0530

    try 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    proxy: {
-      "/britanica/pbl_api": {
-        target: "https://pbl.4edgeit.com",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/britanica/pbl_api": {
+  //       target: "https://pbl.4edgeit.com",
+  //       changeOrigin: true,
+  //       secure: true,
+  //     },
+  //   },
+  // },
 });
