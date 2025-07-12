ffa4c7da559818c456e3ea629bc25ee37c41ff2a
puneethkanike09 <reachout.puneeth@gmail.com>
2025-07-12 12:27:08 +0530

    Revert Real-world connection icon back to 5.svg as requested

--- src/pages/educator/components/common/Header.tsx
+++ src/pages/educator/components/common/Header.tsx
@@ -4,7 +4,7 @@ import ResearchIcon from '../../../../assets/dashboard/Educator/header/1.png';
 import InterdisciplinaryIcon from '../../../../assets/dashboard/Educator/header/22.svg';
 import SkillsIcon from '../../../../assets/dashboard/Educator/header/33.svg';
 import SgdsIcon from '../../../../assets/dashboard/Educator/header/5.png';
-import RealWorldIcon from '../../../../assets/dashboard/Educator/header/4.svg';
+import RealWorldIcon from '../../../../assets/dashboard/Educator/header/5.svg';
 
 export default function BritannicaHeroSection() {
     return (
